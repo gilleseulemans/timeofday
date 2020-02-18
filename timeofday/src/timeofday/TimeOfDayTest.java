143b867c646b519c6e30db038ba3d1db930e9c76
gilleseulemans <56961479+gilleseulemans@users.noreply.github.com>
2020-02-18 08:50:17 +0100

    working encapsuladet

--- timeofday/src/timeofday/TimeOfDayTest.java
+++ timeofday/src/timeofday/TimeOfDayTest.java
@@ -8,32 +8,34 @@ class TimeOfDayTest {
 	
 	class TimeOfDay {
 		/**houd uren en minuten bij */
-		int hours;
-		int minutes;
+		
+		int minutesSinceMidnight;
 	}
 	
 	int getHours(TimeOfDay t) {
-		return t.hours;
+		return t.minutesSinceMidnight / 60;
 	}
 	
 	int getMinutes(TimeOfDay t) {
-		return t.minutes;
+		return t.minutesSinceMidnight % 60;
 	}
 	
 	void setHours(TimeOfDay t, int value) {
-		t.hours = value;
+		t.minutesSinceMidnight %= 60;
+		t.minutesSinceMidnight += value * 60;
 	}
 	
 	void setMinutes(TimeOfDay t, int value) {
-		t.minutes = value;
+		t.minutesSinceMidnight = t.minutesSinceMidnight /60 * 60 + value;
 	}
 
 	@Test
 	void test() {
 		TimeOfDay t = new TimeOfDay();
-		assert t.hours == 0 && t.minutes == 0;
-		t.hours += 12;
-		assert t.hours == 12 && t.minutes == 0;
+		assert getHours(t) == 0 && getMinutes(t) == 0;
+		setHours(t,getHours(t)+12);
+		setMinutes(t,15);
+		assert getHours(t) == 12 && getMinutes(t) == 15;
 		
 	}
 
